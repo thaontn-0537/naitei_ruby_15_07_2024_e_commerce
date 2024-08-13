3e78159893086314eb1d3f21db4b6183aa90e307
thaontn-0537 <nguyen.thi.ngoc.thao@sun-asterisk.com>
2024-08-13 22:38:06 +0700

    [Naitei] E-commerce Seed image data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,11 @@ module ApplicationHelper
     base_title = t "defaults.name"
     page_title.blank? ? base_title : "#{page_title} | #{base_title}"
   end
+
+  def resized_image record
+    return unless record.image.attached?
+
+    record.image.variant(resize_to_fill: [Settings.list.image_size,
+                                          Settings.list.image_size]).processed
+  end
 end
