51571d3639cd16c628c30d58ff2589980d149a14
lqCi-sun <le.quang.chien@sun-asterisk.com>
2024-08-19 07:40:31 +0700

    Search product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,25 @@
 class ProductsController < ApplicationController
+  def search
+    @categories = Category.all
+    @query = params[:q][:product_name_cont]
+    session[:search_query] = @query
+    @pagy, @products_search = pagy(
+      @q.result
+      .search,
+      limit: Settings.page_10
+    )
+    render :search
+  end
+
+  def filter_by_category
+    @categories = Category.all
+    @query = session[:search_query]
+    @filter = Product.ransack(params[:q])
+    @pagy, @products_filtered = pagy(
+      @filter.result
+      .search,
+      limit: Settings.page_10
+    )
+    render :search
+  end
 end
