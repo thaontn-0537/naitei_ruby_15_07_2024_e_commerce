921db4b82f9a4151062e82c2e3dd299ea0700611
lqCi-sun <le.quang.chien@sun-asterisk.com>
2024-09-11 07:26:16 +0700

    Rspec Order model

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,12 +1,14 @@
 FactoryBot.define do
   factory :order do
     association :user
-    total { 5000 }
+    total { rand(100..500) }
     paid_at { Time.zone.now }
     place { "HN" }
     status { 1 }
     user_name { user.user_name }
     user_phone { user.phone }
     payment_method { "momo" }
+    created_at { Time.zone.now }
+    updated_at { Time.zone.now }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,6 @@
 require "spec_helper"
+require "shoulda/matchers"
+require "database_cleaner/active_record"
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -22,5 +24,17 @@ RSpec.configure do |config|
       with.test_framework :rspec
       with.library :rails
     end
+  end
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 end
